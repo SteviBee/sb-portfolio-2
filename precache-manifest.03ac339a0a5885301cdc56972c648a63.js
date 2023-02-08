d48e690e5142c35ac5142f110e05ccbb6a99d8ef
Stephen Borutta <Stephen.Borutta.Dev@gmail.com>
2023-02-08 16:22:29 -0600

    Updates

---
+++ precache-manifest.03ac339a0a5885301cdc56972c648a63.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468b022fa1464131f999d2ebd975cff7",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "fd3883fb00eb4f210100",
+    "url": "/developerFolio/static/css/3.245776da.chunk.css"
+  },
+  {
+    "revision": "bb85c2218246afb97ddc",
+    "url": "/developerFolio/static/css/4.51632a06.chunk.css"
+  },
+  {
+    "revision": "00814cf51b7293bf6cfb",
+    "url": "/developerFolio/static/css/main.e0f8b565.chunk.css"
+  },
+  {
+    "revision": "e6caeb4c542529ac6caf",
+    "url": "/developerFolio/static/js/2.a2780a32.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.a2780a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3883fb00eb4f210100",
+    "url": "/developerFolio/static/js/3.01da6e7b.chunk.js"
+  },
+  {
+    "revision": "bb85c2218246afb97ddc",
+    "url": "/developerFolio/static/js/4.7e77a6e9.chunk.js"
+  },
+  {
+    "revision": "00814cf51b7293bf6cfb",
+    "url": "/developerFolio/static/js/main.abc3021d.chunk.js"
+  },
+  {
+    "revision": "3340cff6506c5b7d7a00",
+    "url": "/developerFolio/static/js/runtime-main.d7911fcc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "4f6b82da333a4471661bd849fde0e8e1",
+    "url": "/developerFolio/static/media/devCarbonLogo.4f6b82da.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "203d34fcba729034c8afc7e3d10db402",
+    "url": "/developerFolio/static/media/lipsLogo.203d34fc.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b9ecd8ecb3ac42e3e1f121786fe20737",
+    "url": "/developerFolio/static/media/probrewerLogo.b9ecd8ec.ico"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "83403cfa7e7dd9c072664fb3f0316f35",
+    "url": "/developerFolio/static/media/springboard-logo-dark@2x.83403cfa.jpeg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "6ca5ab93d7f4b1ba3fd2a46d6764a41f",
+    "url": "/developerFolio/static/media/wwLogo.6ca5ab93.svg"
+  }
+]);
